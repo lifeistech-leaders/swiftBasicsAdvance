ac6c3674bba927c695340c07d12ce75a6a220266
KiyonoOsawa <kiyo.sntr.x6@icloud.com>
2024-05-11 21:19:15 +0900

    Chapter5完了した！！！

--- Chapter5-文字列の連結と埋め込み.playground/Contents.swift
+++ Chapter5-文字列の連結と埋め込み.playground/Contents.swift
@@ -1,36 +1,44 @@
 // 5.1 変数cityとcountryを連結してコンソールに出力してください。
-var city = "San Francisco"
+var city1 = "San Francisco"
 var country = "United States"
+print(city1 + country)
 
 // 5.2 変数fruit1とfruit2を連結してコンソールに出力してください。
 var fruit1 = "apple"
 var fruit2 = "banana"
+print(fruit1 + fruit2)
 
 // 5.3 変数dogとcatを連結してコンソールに出力してください。連結する際、スペースを入れてください。
 var dog = "Golden Retriever"
 var cat = "Bengal"
+print("\(dog) \(cat)")
 
 // 5.4 \()を使用して、変数ageの値を含む文章をコンソールに出力してください。
 var age = 30
+print("彼の年齢は\(age)歳です")
 
 // 5.5 \()を使用して、変数heightの値を含む文章をコンソールに出力してください。
 var height = 180.0
-
+print("彼の身長は\(height)")
 
 // 5.6 \()を使用して、変数temperatureの値を含む文章をコンソールに出力してください。
 var temperature = 25.0
+print("今日の気温は\(temperature)です。")
 
 // 5.7 \()を使用して、変数xとyの和をコンソールに出力してください。
 var x = 5
 var y = 10
+print(x + y)
 
 // 5.8 変数firstNameとlastNameを連結してコンソールに出力してください。連結する際、スペースを入れてください。
 var firstName = "Ada"
 var lastName = "Lovelace"
+print("\(firstName) \(lastName)")
 
 // 5.9 変数streetとcityを連結してコンソールに出力してください。連結する際、カンマとスペースを入れてください。
 var street = "Main Street"
-var city = "New York"
+var city2 = "New York"
+print("\(street), \(city2)")
 
 // 5.10 \()を使用して、変数yearとmonthの値を含む文章をコンソールに出力してください。
 var year = 2023
